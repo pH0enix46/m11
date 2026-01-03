febcc6d1c7744091723973c06b09c2cf1e618510
pH0enix46 <mdjoy.javascript@gmail.com>
2026-01-03 16:09:49 +0600

    working

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/common/Navbar";
+import SmoothScroll from "@/components/ui/SmoothScroll";
 
 const inter = Inter({
   variable: "--font-inter",
@@ -26,6 +27,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${inter.variable} antialiased`}>
         <div className="overflow-x-hidden">
+          <SmoothScroll />
           <Navbar />
           {children}
         </div>
