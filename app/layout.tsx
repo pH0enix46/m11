d77179b9f9250dba72a89c5dfc539548a4f2215c
pH0enix46 <mdjoy.javascript@gmail.com>
2026-01-07 22:30:21 +0600

    working

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,10 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Navbar from "@/components/common/Navbar";
-import Footer from "@/components/common/Footer";
 import SmoothScroll from "@/components/ui/SmoothScroll";
 import { CartProvider } from "@/context/CartContext";
 import { AuthProvider } from "@/context/AuthContext";
+import ConditionalLayout from "@/components/common/ConditionalLayout";
 
 const inter = Inter({
   variable: "--font-inter",
@@ -33,9 +32,7 @@ export default function RootLayout({
           <CartProvider>
             <div className="overflow-x-hidden">
               <SmoothScroll />
-              <Navbar />
-              {children}
-              <Footer />
+              <ConditionalLayout>{children}</ConditionalLayout>
             </div>
           </CartProvider>
         </AuthProvider>
