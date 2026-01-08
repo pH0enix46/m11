98832baaf2aa2f830fe2e60225fb29006d2d422d
Samiul Islam <44663019+samiul16@users.noreply.github.com>
2026-01-08 16:09:29 +0600

    Updated connections

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -58,6 +58,7 @@ const AuthPage = () => {
         setError(data.message || "Invalid credentials");
       }
     } catch (err) {
+      console.log("Auth try Error:", err);
       setError("Connection error. Please try again.");
     } finally {
       setLoading(false);
